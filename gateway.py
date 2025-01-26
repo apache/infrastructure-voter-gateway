57f456cee3527e18ea8f121de7565f86ae5f836b
Daniel Gruno <humbedooh@apache.org>
2025-01-26 15:44:53 +0100

    return a redirect response

--- gateway.py
+++ gateway.py
@@ -4,6 +4,7 @@ import asfquart.generics
 import asfquart.session
 import hashlib
 import asfpy.sqlite
+import quart
 
 CURRENT_ELECTION_ID = "45f66648"
 STEVE_DB = "/var/www/steve/steve-test.db"
@@ -27,7 +28,8 @@ def my_app():
 
         # Check if exists, otherwise add ballot
         ballot_id = await voter_add(CURRENT_ELECTION_ID, session.email, uid_hashed)
-        return f"https://vote.apache.org/election.html?{CURRENT_ELECTION_ID}/{ballot_id}"
+        url = f"https://vote.apache.org/election.html?{CURRENT_ELECTION_ID}/{ballot_id}"
+        return quart.redirect(url)
 
     app.runx(port=8085)
 
